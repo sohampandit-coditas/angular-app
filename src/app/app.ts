84e6e373605da4ea7c4a9d410df407023cb30b39
Coditas-Admin <Coditas-Admin@DESKTOP-PG81ABB>
2026-08-03 10:48:33 +0530

    Creating Application shell using standalone components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { Footer } from './layout/footer/footer';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Dashboard } from './features/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Footer,Sidebar,Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
